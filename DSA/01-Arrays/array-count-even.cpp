5d2ce0ea2f0db1acb4f0af793399385c20dfb6a3
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-26 12:27:32 +0530

    Add array sum program

---
+++ DSA/01-Arrays/array-count-even.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5];
+    int count=0;
+    for(int i=0; i<5; i++)
+    {
+        cin>>arr[i];
+        if(arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    cout<<"count="<<count<<endl;
+    return 0;  
+}

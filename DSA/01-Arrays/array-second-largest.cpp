dd5abb463d51185180e049ddf381cd51e219fee3
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-26 15:06:47 +0530

    Add second largest array program

---
+++ DSA/01-Arrays/array-second-largest.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+
+    cin >> arr[0];
+
+    int max = arr[0];
+    int secondMax = arr[0];
+
+    for(int i = 1; i < 5; i++)
+    {
+        cin >> arr[i];
+
+        if(arr[i] > max)
+        {
+            secondMax = max;
+            max = arr[i];
+        }
+    }
+
+    cout << "Second Largest = " << secondMax;
+
+    return 0;
+}

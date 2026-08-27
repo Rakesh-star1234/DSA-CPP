6f69315dc0d9da2b8db090d191337c3b236a3621
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-27 15:43:07 +0530

    Add Count negative  even numbers

---
+++ DSA/01-Arrays/array-negative-even-count.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5];
+    int negativeEvenCount=0;
+    for(int i=0; i<5; i++)
+    {
+        cin>>arr[i];
+        if(arr[i]<0 && arr[i]%2==0)
+        {
+            negativeEvenCount++;
+
+        }
+    }
+    cout<<"Number of negative even elements:"<<negativeEvenCount<<endl;
+    return 0;
+}

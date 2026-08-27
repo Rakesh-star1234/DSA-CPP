6a0376ccfcdc43c7a47324099b445bd23f7921ce
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-27 15:56:17 +0530

    Add first even numbers

---
+++ DSA/01-Arrays/array-first-even.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+    int firstEven = 0;
+    int found = 0;
+
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+
+        if(arr[i] % 2 == 0)
+        {
+            firstEven = arr[i];
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 1)
+    {
+        cout << "First Even = " << firstEven;
+    }
+    else
+    {
+        cout << "No even element found";
+    }
+
+    return 0;
+}

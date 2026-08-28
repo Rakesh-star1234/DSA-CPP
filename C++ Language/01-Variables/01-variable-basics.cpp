c185074de51296867859f35f372da7b6e93bb9e8
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-28 22:55:37 +0530

    Added C++ Language variables practice

---
+++ C++ Language/01-Variables/01-variable-basics.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age=24;
+    int marks=90;
+    cout<<"age="<<age<<endl;
+    cout<<"marks="<<marks<<endl;
+    return 0;
+}
---
+++ C++ Language/01-Variables/02-variable-change.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age=24;
+    int marks=90;
+    cout<<"age="<<age<<endl;
+    cout<<"marks="<<marks<<endl;
+    age=25;
+    marks=95;
+    cout<<"age="<<age<<endl;
+    cout<<"marks="<<marks<<endl;
+    return 0;
+}

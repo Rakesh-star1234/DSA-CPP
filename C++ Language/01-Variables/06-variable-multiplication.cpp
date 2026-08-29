55edec3101d42057b99ac7545ae678d967c0a767
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-29 18:02:36 +0530

    Added variable multiplication practice

---
+++ C++ Language/01-Variables/06-variable-multiplication.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=12;
+    int b=6;
+    int c=a*b;
+    cout<<"a= "<<a<<endl; cout<<"b= "<<b<<endl; cout<<"c= "<<c<<endl;
+    return 0; 
+}

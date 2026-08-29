6431420d32fc0793f0228065c91996fc5d7626d1
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-29 10:59:13 +0530

    Added C++ variable addition practice

---
+++ C++ Language/01-Variables/03-variable-addition.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age=24;
+    int marks=90;
+    cout<<"age="<<age<<endl;
+    cout<<"marks="<<marks<<endl;
+    int total=age+marks;
+    cout<<"total="<<total<<endl; 
+    return 0;
+}

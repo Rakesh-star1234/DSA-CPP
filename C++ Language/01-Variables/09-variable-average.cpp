b3c28191724f7cfd9db7c847d33a43a662edefc6
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-29 18:23:08 +0530

    Added variable average practice

---
+++ C++ Language/01-Variables/09-variable-average.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    int b=20;
+    int c=30;
+    int average=(a+b+c)/3;
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    cout<<"c="<<c<<endl;
+    cout<<"average="<<average<<endl;
+    return 0; 
+}

e8321b3e8c251164f52019f323109c8a2d3cb25c
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-29 11:05:43 +0530

    Added C++ variable input practice

---
+++ C++ Language/01-Variables/04-variable-input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age=24;
+    int marks=90;
+    cin>>age;
+    cin>>marks;
+    cout<<"age="<<age<<endl;
+    cout<<"marks="<<marks<<endl;
+    return 0; 
+}

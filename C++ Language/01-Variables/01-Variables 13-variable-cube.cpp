a6e0b2f17889fd9bc42c106e91d1818b8add67e4
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-29 19:59:38 +0530

    Added cube of number practice

---
+++ C++ Language/01-Variables/01-Variables 13-variable-cube.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 5;
+    int cube = number * number * number;
+
+    cout << "Number: " << number << endl;
+    cout << "Cube: " << cube << endl;
+
+    return 0; 
+}

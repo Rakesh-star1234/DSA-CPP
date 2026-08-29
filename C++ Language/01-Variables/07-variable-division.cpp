a2ed93fe6c292cf737fb8d5108e41c4d55903ecb
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-29 18:11:41 +0530

    Added variable division practice

---
+++ C++ Language/01-Variables/07-variable-division.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=12;
+    int b=6;
+    int result= a/b;
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    cout<<"result="<<result<<endl;
+    return 0; 
+}

31eb3e02c021291b24a9e327279956a6bc152e37
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-29 18:34:05 +0530

    Added rectangle area variable practice

---
+++ C++ Language/01-Variables/10-variable-rectangle-area.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Length=10;
+    int width=5;
+    int area=Length*width;
+    cout<<"Length="<<Length<<endl;
+    cout<<"Width="<<width<<endl;
+    cout<<"Area="<<area<<endl;
+    return 0;
+}

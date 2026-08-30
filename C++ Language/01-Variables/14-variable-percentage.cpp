406cf576a513a6088e1262662da29dccae3293c0
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-30 10:56:37 +0530

    Added percentage practice

--- C++ Language/01-Variables/14-variable-percentage.cpp
+++ C++ Language/01-Variables/14-variable-percentage.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    int hindi=80;
-    int english=90;
-    int maths=70;
-    int science=85;
-    int socialScience=75;
-    int Total=(hindi+english+maths+science+socialScience);
-    int Total_percentage(Total/5);
-    cout<<"Total marks:"<<Total<<endl;
-    cout<<"Total prcentage:"<<Total_percentage<<endl; 
+    int hindi = 80;
+    int english = 90;
+    int maths = 70;
+    int science = 85;
+    int socialScience = 75;
+
+    int total = hindi + english + maths + science + socialScience;
+    int percentage = (total * 100) / 500;
+
+    cout << "Total: " << total << endl;
+    cout << "Percentage: " << percentage << "%" << endl;
+
     return 0;
 }

82afc0249163e9e84ad52a7eb2493b9058902725
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-31 11:11:58 +0530

    Added even odd check practice

---
+++ C++ Language/01-Variables/24-variable-even-odd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 24;
+
+    if (number % 2 == 0)
+    {
+        cout << "Even" << endl;
+    }
+    else
+    {
+        cout << "Odd" << endl;
+    }
+
+    return 0;  
+}

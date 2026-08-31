bd99bcbee828158ad35d7a795552e4e578162e33
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-31 10:16:06 +0530

    Added last digit practice

--- C++ Language/01-Variables/19-variable-last-digit.cpp
+++ C++ Language/01-Variables/19-variable-last-digit.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int number =1234;
-    int lastDigit= number% 10;
-    cout<<"lastDigit:"<<lastDigit<<endl;
-    return 0; 
+int main()
+{
+    int number = 1234;
+    int lastDigit = number % 10;
+
+    cout << "Last digit: " << lastDigit << endl;
+
+    return 0;
+} 

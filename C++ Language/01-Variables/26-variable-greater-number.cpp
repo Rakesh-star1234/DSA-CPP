bcceed2f9abb6933e7765cd83770809554a54fe2
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-31 16:14:49 +0530

    Added greater number practice

---
+++ C++ Language/01-Variables/26-variable-greater-number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 23; 
+    int b = 24;
+
+    if (a > b)
+    {
+        cout << "Greater: " << a << endl;
+    }
+    else
+    {
+        cout << "Greater: " << b << endl;
+    }
+
+    return 0;
+}

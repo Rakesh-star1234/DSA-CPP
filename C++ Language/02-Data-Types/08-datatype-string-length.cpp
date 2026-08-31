973b1fb2385278e7edfb6127320e92205d306979
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-08-31 17:44:28 +0530

    Added string length practice

---
+++ C++ Language/02-Data-Types/08-datatype-string-length.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name="Rakesh";
+   
+    cout<<"name;"<<name<<endl; 
+    cout<<"length:"<<name .length();   
+    return 0; 
+} 

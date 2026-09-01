a7d33b87f94d079c89ce15627a87aed0943189ff
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-01 16:07:17 +0530

    Added multiple data types practice

---
+++ C++ Language/02-Data-Types/09-datatype-multiple.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int age=20;
+    float temperature=32.3;
+    char firstLetter='R';
+    bool passed=true;
+    string name="Rakesh Kumar";
+    cout<<"age:"<<age<<endl;
+    cout<<"temperature:"<<temperature<<endl;
+    cout<<"firstLetter:"<<firstLetter<<endl;
+    cout<<"passed:"<<passed<<endl;
+    cout<<"name:"<<name<<endl;
+    return 0;
+
+}

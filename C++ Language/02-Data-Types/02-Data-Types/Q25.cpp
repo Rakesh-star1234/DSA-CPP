a59a734c82dddecf37d75fbf871b176409b3c30f
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-01 23:31:05 +0530

    Added temperature conversion practice Q25

---
+++ C++ Language/02-Data-Types/02-Data-Types/Q25.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float celsiustemperature=37.5;
+    float fahraheittemperature=(celsiustemperature*9/5)+32;
+    cout<<"celsiustemperature:"<<celsiustemperature<<endl;
+    cout<<"fahraheittemperature:"<<fahraheittemperature<<endl;
+    return 0; 
+}

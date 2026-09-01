615c88ff364cfe54bcb562afa5316dc400e979da
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-01 23:24:43 +0530

    Added data type selection practice Q24

---
+++ C++ Language/02-Data-Types/02-Data-Types/Q24.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age =20;
+    float marks = 87.5;
+    char grade = 'A';
+    bool passed = true;
+    cout<<"age:"<<age<<endl;
+    cout<<"marks:"<<marks<<endl;
+    cout<<"grade:"<<grade<<endl;
+    cout<<"passed:"<<passed<<endl;
+    return 0; 
+
+} 

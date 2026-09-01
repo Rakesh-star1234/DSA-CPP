2511538bb46a139c50a9fcf66c2e3aa4ecb18c88
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-01 17:36:50 +0530

    Added integer subtraction practice

---
+++ C++ Language/02-Data-Types/18-datatype-subtraction.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 50;
+    int b = 20;
+    int difference = a - b;
+    cout << "Difference: " << difference << endl;
+    return 0;
+}

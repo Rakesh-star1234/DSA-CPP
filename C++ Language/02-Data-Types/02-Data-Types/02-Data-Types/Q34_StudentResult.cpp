3d4f9f23776e05309aa93487b327ae30089ac035
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-02 18:06:59 +0530

    Added student result data types practice Q34

---
+++ C++ Language/02-Data-Types/02-Data-Types/02-Data-Types/Q34_StudentResult.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name=" Rakesh Kumar";
+    int age=23;
+    float marks=85.5;
+    char grade='A';
+    bool passed=true;
+    cout<<"Name:"<<name<<endl;
+    cout<<"Age:"<<age<<endl;
+    cout<<"Marks:"<<marks<<endl;
+    cout<<"Grade:"<<grade<<endl;
+    cout<<"Passed:"<<passed<<endl;
+    return 0; 
+}

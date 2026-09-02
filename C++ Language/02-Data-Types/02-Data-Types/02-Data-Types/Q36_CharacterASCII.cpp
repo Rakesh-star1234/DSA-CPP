6d1b05753543209b67d92032b0e0a70ebef1c67b
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-02 19:09:50 +0530

    Added character ASCII practice Q36

---
+++ C++ Language/02-Data-Types/02-Data-Types/02-Data-Types/Q36_CharacterASCII.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char character ='A';
+    int asciiValue = character;
+    cout << "Character: " << character << endl;
+    cout << "ASCII Value: " << asciiValue << endl;
+    return 0; 
+}

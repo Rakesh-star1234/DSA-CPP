519a67909768eb553cad58ed6c47a19b3b811675
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-02 20:45:58 +0530

    Added float vs double precision practice Q37

---
+++ C++ Language/02-Data-Types/02-Data-Types/02-Data-Types/Q37_FloatVsDouble.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float priceFloat = 999.987654321;
+    double priceDouble = 999.987654321;
+    cout << "Float Price: " << priceFloat << endl;
+    cout << "Double Price: " << priceDouble << endl;
+    return 0;
+}

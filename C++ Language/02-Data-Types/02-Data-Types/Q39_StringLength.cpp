bee3aad2af07f28abbff965f80ff447adf4038a3
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-02 21:06:36 +0530

    Added string length practice Q39

---
+++ C++ Language/02-Data-Types/02-Data-Types/Q39_StringLength.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name = "Rakesh Kumar";
+    int length = name.length();
+    cout <<"Name:"<< name <<endl;
+    cout << "Length of the string: " << length << endl;
+    return 0; 
+}

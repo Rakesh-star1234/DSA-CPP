fffd87832c4dac210ca83db5872d6f6c9ace4e9c
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-02 21:21:40 +0530

    Added arithmetic operators practice Q1

---
+++ C++ Language/03-Operators/Q1_ArithmeticOperators.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=25;
+    int b=7;
+    int sum=a+b;
+    int difference=a-b;
+    int product=a*b;
+    int division=a/b;
+    int remainder=a%b;
+    cout<<"Sum: "<<sum<<endl;
+    cout<<"Difference: "<<difference<<endl;
+    cout<<"Product: "<<product<<endl;
+    cout<<"Division: "<<division<<endl;
+    cout<<"Remainder:"<<remainder<<endl;
+
+    return 0; 
+}

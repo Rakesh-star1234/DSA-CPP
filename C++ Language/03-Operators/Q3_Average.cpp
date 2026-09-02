bbfc946316931c593187176a1ce90ad079658fa4
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-02 21:31:08 +0530

    Added average calculation practice Q3

---
+++ C++ Language/03-Operators/Q3_Average.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    float average = (a + b + c) / 3.0;
+    cout << "Average: " << average << endl;
+    return 0;
+}

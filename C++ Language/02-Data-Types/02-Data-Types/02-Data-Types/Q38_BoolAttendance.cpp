af5776d65f917e0258563f8a229fd7f0feee7024
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-02 20:56:39 +0530

    Added bool attendance practice Q38

---
+++ C++ Language/02-Data-Types/02-Data-Types/02-Data-Types/Q38_BoolAttendance.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string StudentName ="RakeshKumar";
+    bool present =true;
+    cout<<"Student Name:"<<StudentName<<endl;
+    cout<<"present:"<<present<<endl;
+    return 0;
+    
+}

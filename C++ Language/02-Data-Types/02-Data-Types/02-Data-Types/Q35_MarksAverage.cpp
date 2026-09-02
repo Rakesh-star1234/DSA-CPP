5e95738e4fdfe751eeb41ee4d642fae763419f8a
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-02 18:25:46 +0530

    Added marks average type conversion practice Q35

---
+++ C++ Language/02-Data-Types/02-Data-Types/02-Data-Types/Q35_MarksAverage.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int English=78;
+    int Maths=85;
+    int Science=92;
+    int TotalMarks=English+Maths+Science;
+    float AverageMarks=TotalMarks/3.0;
+    cout<<"English marks:"<<English<<endl;
+    cout<<"Maths marks:"<<Maths<<endl;
+    cout<<"Science marks:"<<Science<<endl;
+    cout<<"Total marks:"<<TotalMarks<<endl;
+    cout<<"Average marks:"<<AverageMarks<<endl;
+    return 0;
+}

d51698210e8cb2ec4fb80262796280c7a41600b0
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-03 22:46:52 +0530

    Added division and remainder practice Q14

---
+++ C++ Language/03-Operators/Q14_Division_Remainder.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int a = 37;
+    int b = 6;
+    int quotient = a / b;
+    int remainder = a % b;
+    cout << "Quotient: " << quotient << endl;
+    cout << "Remainder: " << remainder << endl;
+    return 0;
+}

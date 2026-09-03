bfe67118129b2c6358e61a8a572df297e3a2fdce
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-03 17:53:06 +0530

    Added operator precedence practice Q10

---
+++ C++ Language/03-Operators/Q10_Operator_Precedence.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+    int a = 10;
+    int b = 5;
+    int c = 2;
+    int result =a+b*c;
+    cout<<"Result:"<<result<<endl; 
+    return 0;
+}

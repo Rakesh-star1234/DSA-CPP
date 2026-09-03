6d1d2176c2dc10ccdad30030504ca820f45d4061
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-03 10:38:39 +0530

    Added arithmetic and relational operators practice Q6

---
+++ C++ Language/03-Operators/Q6_ArithmeticRelational.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=50;
+    int b=30;
+    int sum=a+b;
+    int difference=a-b;
+    bool greater=a>b;
+    cout<<"sum:"<<sum<<endl;
+    cout<<"difference:"<<difference<<endl;
+    cout<<"A greater than B:"<<greater<<endl;
+    return 0; 
+
+}

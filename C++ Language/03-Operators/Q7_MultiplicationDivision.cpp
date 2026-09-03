5e87c16521076c87bd4297b645b22632c27a2c2c
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-03 10:43:05 +0530

    Added multiplication and division practice Q7

---
+++ C++ Language/03-Operators/Q7_MultiplicationDivision.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=60;
+    int b=30;
+    int product=a*b;
+    int quotient=a/b;
+    cout<<"product:"<<product<<endl;
+    cout<<"quotient:"<<quotient<<endl;
+    return 0;
+}

bc6808495da8dfc01046911cc92901a85eb30b44
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-04 15:19:33 +0530

    Added logical NOT operators practice Q18

---
+++ C++ Language/03-Operators/Q18_Logical_NOT.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool isPassed = true;
+    bool isFailed = !isPassed;
+    cout << "Is Passed: " << isPassed << endl;
+    cout << "Is Failed: " << isFailed << endl;
+    return 0;  
+}

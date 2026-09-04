43ebd917f51121cbad59788419c7d2fe6d0ca759
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-04 15:15:29 +0530

    Added logical OR operators practice Q17

---
+++ C++ Language/03-Operators/Q16_Logical_AND.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int age=22;
+    int marks=65;
+    bool result1=age>=18 && marks>=40;
+    cout << "Result 1: " << result1 << endl;
+    return 0;
+}
---
+++ C++ Language/03-Operators/Q17_Logical_OR.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age=16;
+    int marks=75;
+    bool result1 = age>=18 || marks>=40;
+    cout <<"Result 1:" << result1 << endl;
+    return 0;
+} 

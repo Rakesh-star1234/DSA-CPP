45f712cd9a041b587205cb6850c90d9f78423328
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-04 15:32:46 +0530

    Added unary plus and minus operators practice Q20

---
+++ C++ Language/03-Operators/Q20-Unary-Plus-Minus.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 15;
+    int positive = +x;
+    int negative = -x;
+    cout<<"Value of x:"<<x<<endl;
+    cout<<"Positive: "<<positive<<endl;
+    cout<<"Negative: "<<negative<<endl;
+    return 0;
+
+} 

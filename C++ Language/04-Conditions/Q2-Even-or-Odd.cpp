7ced8d52a9ec2af296adbe3e0ee8d01ef933cf33
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-05 21:54:49 +0530

    Added even or odd condition practice Q2

---
+++ C++ Language/04-Conditions/Q2-Even-or-Odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Number=25;
+    if(Number%2==0)
+    {
+        cout<<"Even:"<<endl;
+    }
+    else
+    {
+        cout<<"Odd:"<<endl;
+    }
+    return 0;
+}

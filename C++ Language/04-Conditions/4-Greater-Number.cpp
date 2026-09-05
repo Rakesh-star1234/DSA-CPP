ae31c1c4f1d92b62df9dcf1f72d40af6a199e4cf
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-05 22:11:12 +0530

    Added greater number condition practice Q4

---
+++ C++ Language/04-Conditions/4-Greater-Number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A = 35;
+    int B = 20;
+
+    if(A > B)
+    {
+        cout << "A is Greater" << endl;
+    }
+    else if(B > A)
+    {
+        cout << "B is Greater" << endl;
+    }
+    else
+    {
+        cout << "Both are Equal" << endl;
+    }
+
+    return 0;
+} 

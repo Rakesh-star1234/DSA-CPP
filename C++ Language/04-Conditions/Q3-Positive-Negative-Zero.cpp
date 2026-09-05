bbf5693db34af39e83c394d40a9879408e271b80
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-05 22:03:48 +0530

    Added positive negative zero condition practice Q3

---
+++ C++ Language/04-Conditions/Q3-Positive-Negative-Zero.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Number= -15;
+    if(Number>0) 
+    {
+        cout<<"Positive:"<<endl;
+
+    }
+    else if(Number < 0) 
+    {
+        cout<<"Negetive:"<<endl;
+    }
+    else
+    {
+        cout<<"Zero:"<<endl; 
+    }
+    return 0; 
+}

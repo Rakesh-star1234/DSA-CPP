d6f31b4a9e0a389968f31bf1a598a06b21bc6b2c
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-05 16:51:54 +0530

    Added electricity bill practice Medium Q4

---
+++ C++ Language/03-Operators/Q25-Medium-Electricity-Bill.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Units = 250;
+    int RatePerUnit = 6;
+    int TotalBill = Units * RatePerUnit;
+    cout << "Total Electricity Bill: ₹" << TotalBill << endl;
+    return 0;
+}

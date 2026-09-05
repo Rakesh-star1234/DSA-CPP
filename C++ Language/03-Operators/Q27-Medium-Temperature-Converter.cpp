179ae0129eb19d29e78cd4428f6f5a4b86590ad9
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-05 17:01:50 +0530

    Added temperature converter practice Medium Q6

---
+++ C++ Language/03-Operators/Q27-Medium-Temperature-Converter.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Celsius = 37;
+    float fahrenheit = (Celsius *9.0/5.0) + 32;
+    cout <<"Celsius:"<<Celsius<<endl;
+    cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+    return 0;
+}

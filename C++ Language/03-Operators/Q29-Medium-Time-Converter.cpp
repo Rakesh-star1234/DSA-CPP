3bdd9b98f461c40399a4ad2ab27cf48ec4cd41e8
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-05 21:44:44 +0530

    Added time converter practice Medium Q8

---
+++ C++ Language/03-Operators/Q29-Medium-Time-Converter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int TotalMinutes = 135;
+    int Hours=TotalMinutes/60;
+    int Minutes=TotalMinutes%60;
+    cout<<"Total Minutes:"<<TotalMinutes<<endl;
+    cout<<"Hours:"<<Hours<<endl;
+    cout<<"Minutes:"<<Minutes<<endl;
+    return 0; 
+} 

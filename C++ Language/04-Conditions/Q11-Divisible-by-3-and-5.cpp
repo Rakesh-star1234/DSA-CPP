64b4eccf8c5902547876f705a40ea21e08e8aa8e
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-06 18:12:36 +0530

    Added divisible by 3 and 5 condition practice Q11

---
+++ C++ Language/04-Conditions/Q11-Divisible-by-3-and-5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Number = 30;
+    if(Number%3==0 && Number%5==0)
+    {
+        cout<<"Divisibal by 3 and 5:"<<endl;
+    }
+    else
+    {
+
+    cout<<"Not Divisival :"<<endl;
+    }
+    return 0;
+
+}

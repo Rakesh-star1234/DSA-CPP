f7a5cd2dfc0ede24368bf871b331222d207c6021
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-06 17:52:39 +0530

    Added voting eligibility condition practice Q9

---
+++ C++ Language/04-Conditions/Q9-Voting-Eligibility.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Age=20;
+    if(Age>=18)
+    {
+        cout<<"Eligible:"<<endl;
+    }
+    else
+    {
+        cout<<"Not Eligible:"<<endl;
+    }
+    return 0; 
+}

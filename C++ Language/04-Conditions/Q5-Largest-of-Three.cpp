c362a613e698bf4a4d81e03c46714d9640ca185f
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-06 11:59:09 +0530

    Added largest of three numbers condition practice Q5

---
+++ C++ Language/04-Conditions/Q5-Largest-of-Three.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A = 25;
+    int B = 40;
+    int C = 30;
+
+    if(A > B && A > C)
+    {
+        cout << "Largest = " << A << endl;
+    }
+    else if(B > A && B > C)
+    {
+        cout << "Largest = " << B << endl;
+    }
+    else
+    {
+        cout << "Largest = " << C << endl;
+    }
+
+    return 0;
+}

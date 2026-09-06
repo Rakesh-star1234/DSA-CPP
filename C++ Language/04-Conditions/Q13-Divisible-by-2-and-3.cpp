4963911068108b5cca715e10dacd119be798f592
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-06 18:30:11 +0530

    Added divisible by 2 and 3 condition practice Q13

---
+++ C++ Language/04-Conditions/Q13-Divisible-by-2-and-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Number = 18;
+    if(Number%2==0 && Number%3==0)
+    {
+        cout<<"Divisival by 2 and 3:"<<endl;
+    }
+    else
+    {
+        cout<<"Not Divisival :"<<endl;
+    } 
+    return 0;
+}

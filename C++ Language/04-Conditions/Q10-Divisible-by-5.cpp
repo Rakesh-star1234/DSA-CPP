ff6783681598fa543c18749a93f3680459bac799
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-06 18:14:16 +0530

    Added divisible by 5 condition practice Q10

---
+++ C++ Language/04-Conditions/Q10-Divisible-by-5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Number=35;
+    if(Number%5==0) 
+    {
+        cout<<"Divisival by 5:"<<endl;
+    }
+    else 
+    {
+        cout<<" Not Divisival by 5:"<<endl;  
+    }
+
+}

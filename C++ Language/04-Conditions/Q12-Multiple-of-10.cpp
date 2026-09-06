fe87d3a6d287facdad09b27c43c650acea41cc3f
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-06 18:24:05 +0530

    Added multiple of 10 condition practice Q12

---
+++ C++ Language/04-Conditions/Q12-Multiple-of-10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Number = 70;
+    if(Number%10==0)
+    {
+        cout<<"Multiple of 10:"<<endl;
+    }
+    else
+    {
+    cout<<"Not Multiple of 10: "<<endl;
+    }
+    return 0;
+
+}

04d6a4bcd4ec6118eb4d2d068448401bbf3d2cb3
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-07 22:31:32 +0530

    Added number in range condition practice Q14

---
+++ C++ Language/04-Conditions/Q14-Number-in-Range.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Number = 75;
+
+    if(Number >= 50 && Number <= 100)
+    {
+        cout << "Number is in range" << endl;
+    }
+    else
+    {
+        cout << "Number is not in range" << endl;
+    }
+
+    return 0;
+}

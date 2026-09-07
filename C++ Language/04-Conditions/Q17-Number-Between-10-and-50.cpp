864c88bf8f7ca2742a8836b1145163d44fe91b73
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-07 22:41:45 +0530

    Added number between 10 and 50 condition practice Q17

---
+++ C++ Language/04-Conditions/Q17-Number-Between-10-and-50.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Number = 30;
+
+    if(Number >= 10 && Number <= 50)
+    {
+        cout << "Number is between 10 and 50" << endl;
+    }
+    else
+    {
+        cout << "Number is not between 10 and 50" << endl;
+    }
+
+    return 0;
+}

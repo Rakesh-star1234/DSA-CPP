06ef407d987c04ae71c6b681659a9de859143f0b
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-09 18:52:26 +0530

    Added student pass or fail condition practice Q26

---
+++ C++ Language/04-Conditions/Q26-Student-Pass-or-Fail.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Marks=65;
+    if(Marks>=40)
+    {
+      cout<<"Pass:"<<endl;
+    }
+    else
+    {
+        cout<<"Fail:"<<endl; 
+    }
+    return 0;
+    
+} 

2b3144c348df967d2fe0d241d4d948bb4be659df
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-09 22:26:20 +0530

    Added driving license eligibility condition practice Q28

---
+++ C++ Language/04-Conditions/28-Driving-License-Eligibility.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Age = 19;
+    if(Age>=18)
+    {
+        cout<<"Eligible for Driving License:"<<endl;
+    }
+    else
+    {
+        cout<<"Not Eligible for Driving License:"<<endl;
+    }
+    return 0;
+}

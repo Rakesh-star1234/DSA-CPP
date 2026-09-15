b4446bf855a9f9b6d36d23c27176bddf444a5965
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-15 22:53:51 +0530

    Added exam result and grade condition practice Q47

---
+++ C++ Language/04-Conditions/Q47-Exam-Result-and-Grade.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Marks = 86;
+
+    if(Marks >= 90)
+    {
+        cout << "Grade A" << endl;
+    }
+    else if(Marks >= 80)
+    {
+        cout << "Grade B" << endl;
+    }
+    else if(Marks >= 70)
+    {
+        cout << "Grade C" << endl;
+    }
+    else if(Marks >= 60)
+    {
+        cout << "Grade D" << endl;
+    }
+    else
+    {
+        cout << "Fail" << endl;
+    }
+
+    return 0;
+}

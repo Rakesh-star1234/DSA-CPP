c915323d75a968f07ac2c6be9bd18bd1ca4a67b5
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-18 22:42:38 +0530

    Added loop practice Q5 multiplication table

---
+++ C++ Language/05-Loops/Q5-Multiplication-Table.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Number = 5;
+
+    for(int i = 1; i <= 10; i++)
+    {
+        cout << Number << " x " << i << " = " << Number * i << endl;
+    }
+
+    return 0;
+}

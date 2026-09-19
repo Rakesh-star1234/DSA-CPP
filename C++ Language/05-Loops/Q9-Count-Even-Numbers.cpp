357fba38535c3dc3b57b5357fb8062960c3b2581
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-19 11:24:45 +0530

    Added loop practice Q9 count even numbers

---
+++ C++ Language/05-Loops/Q9-Count-Even-Numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count = 0;
+
+    for(int i = 1; i <= 20; i++)
+    {
+        if(i % 2 == 0)
+        {
+            count = count + 1;
+        }
+    }
+
+    cout << "Even Count = " << count << endl;
+
+    return 0;
+}

207eaed27ce7706f3ce0911e0a0153783cc6e3e4
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-20 22:36:31 +0530

    Added loop practice Q14 sum greater than 5

---
+++ C++ Language/05-Loops/Q14-Sum-Greater-Than-5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+
+    for(int i = 1; i <= 10; i++)
+    {
+        if(i > 5)
+        {
+            sum = sum + i;
+        }
+    }
+
+    cout << "Sum: " << sum << endl;
+
+    return 0;
+}

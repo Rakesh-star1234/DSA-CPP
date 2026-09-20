e0b573cb5b7fa80f130c5ddaecf9fc3c2ffe9ff8
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-20 17:33:53 +0530

    Added loop practice Q10 count odd numbers

---
+++ C++ Language/05-Loops/Q10-Count-Odd-Numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int count =0;
+    for(int i=1; i<=20; i++)
+    if(i % 2 != 0)
+    count=count+1;
+    cout<<"count:"<<count<<endl;
+    return 0; 
+}

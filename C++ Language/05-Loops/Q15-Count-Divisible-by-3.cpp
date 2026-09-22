648f1737b661b21e336d69199227c27999666f5b
Rakesh Kumar <rakeshkumar906559@gmail.com>
2026-09-22 16:39:32 +0530

    Added loop practice Q15 count divisible by 3

---
+++ C++ Language/05-Loops/Q15-Count-Divisible-by-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Count=0;
+    for(int i=1;i<=20; i++)
+    {
+
+    if(i%3==0)
+    {
+
+    Count=Count+1;
+    }
+} 
+    cout<<"Count:"<<Count<<endl; 
+    return 0; 
+}
